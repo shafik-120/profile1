935e84e2ab9477c7760609fa6b3aee597ddb7814
shafikul <shaifk108@gmail.com>
2023-12-11 17:10:03 +0600

    link kora complete

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
 import { RouterView } from "vue-router";
-import HomeView from "./views/HomeView.vue";
 </script>
 
 <template>
-  <!-- <HomeView/> -->
   <router-view></router-view>
 </template>
 
